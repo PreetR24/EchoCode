741837cb2009b66312901de8758191d21c27a214
Preet Rupapara <preetrupapara247@gmail.com>
2025-08-22 10:36:46 +0530

    minor updates

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,10 @@
-// import React from "react"
 import ReactDOM from "react-dom/client"
-// Import global styles first
-import "@/styles/global.css"
-import App from "./App.tsx"
-import AppProvider from "./context/AppProvider.tsx"
+import "./styles/global.css"
+import App from "./App"
+import AppProvider from "./context/AppProvider"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    // <React.StrictMode>
     <AppProvider>
         <App />
-    </AppProvider>,
-    // </React.StrictMode>
+    </AppProvider>
 )
